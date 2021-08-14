745f98a2af3ba1269594318b897f72c3c66bfc3c
Anushka-shukla <shuklaannushka@gmail.com>
2021-08-15 00:10:20 +0530

    leetcode-FindEvenDigitsInNumsArr

--- src/com/company/daysofcode/arrays/FindEvenDigitsInNums.java
+++
@@ -1,7 +0,0 @@
-package com.company.daysofcode.arrays;
-
-public class FindEvenDigitsInNums {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/com/company/daysofcode/arrays/LeetCode/FindEvenDigitsInNums.java
@@ -0,0 +1,52 @@
+package com.company.daysofcode.arrays.LeetCode;
+
+// https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
+
+import java.util.Scanner;
+
+public class FindEvenDigitsInNums {
+    public static void main(String[] args) {
+        int[] nums = {12,345,2,6,7896};
+        int ans = findNumbers(nums);
+        System.out.println(ans);
+
+    }
+
+    static int findNumbers(int[] nums) {
+        int count = 0;
+        for(int num =0; num < nums.length ;num++){
+            if(even(nums[num]) == true){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    // function to check whether a number contains even number of digits or not
+    static boolean even(int num){
+        if(NumOfDigits(num) % 2 == 0){
+            return true;
+        }
+        return false;
+    }
+
+   // function to count number of digits in a given number
+    static int NumOfDigits(int num){
+        if(num < 0){
+            num = num * -1;
+        }
+
+        if(num == 0){
+            return 1;
+        }
+
+        int count =0;
+        while(num>0){
+            num = num/10;
+            count++;
+        }
+        return count;
+    }
+
+
+}
